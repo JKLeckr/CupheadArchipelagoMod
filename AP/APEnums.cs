c929f5a56d6897e40ca8d87e33ccbd1188360625
JKLeckr <11635283+JKLeckr@users.noreply.github.com>
2023-10-11 17:34:16 -0500

    Trimmed out some fat to put on some more... and added APSettings

---
+++ AP/APEnums.cs
@@ -0,0 +1,9 @@
+namespace CupheadArchipelago.AP {
+    public enum GradeChecks {
+        Disabled = 0,
+        AGrade = 1,
+        APlusGrade = 2,
+        SGrade = 3,
+        Pacifist = 4,
+    }
+}
---
+++ AP/APSettings.cs
@@ -0,0 +1,10 @@
+namespace CupheadArchipelago.AP {
+    public class APSettings {
+        public static bool UseDLC { get; internal set; } = false;
+        public static bool Hard { get; internal set; } = false;
+        public static bool FreemoveIsles { get; internal set; } = false;
+        public static GradeChecks BossGradeChecks { get; internal set; } = GradeChecks.Disabled;
+        public static GradeChecks RungunGradeChecks { get; internal set; } = GradeChecks.Disabled;
+        public static bool DeathLink { get; internal set; } = false;
+    }
+}
