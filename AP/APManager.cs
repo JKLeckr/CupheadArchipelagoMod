549814177f3198bc5b265d02785ad1fb2946a41a
JKLeckr <11635283+JKLeckr@users.noreply.github.com>
2023-11-02 13:46:37 -0500

    Completed the location map. Now get the checks system and item recieving straightened out

---
+++ AP/APManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace CupheadArchipelago.AP {
+    public class APManager : MonoBehaviour {
+        public static APManager Current {get; private set;}
+
+        void Awake() {
+            if (Current != null) {
+                Destroy(gameObject);
+                return;
+            }
+        }
+
+        void Update() {
+            if (!PlayerData.inGame) {
+                Plugin.Log("[APManager] Outside game. Dying off.");
+            }
+        }
+
+        void OnDestroy() {
+            if (Current == this) Current = null;
+            Plugin.Log("[APManager] Destroy");
+        }
+    }
+}
