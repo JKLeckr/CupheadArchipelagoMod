8c46aa57afbd4be13d8c11b42912952edf3eb2e5
JKLeckr <11635283+JKLeckr@users.noreply.github.com>
2023-12-30 19:52:07 -0600

    Getting to the bottom of the coin delemma once and for all!

---
+++ Hooks/MapCoinHook.cs
@@ -0,0 +1,17 @@
+using HarmonyLib;
+
+namespace CupheadArchipelago.Hooks {
+    public class MapCoinHook {
+        public static void Hook() {
+            Harmony.CreateAndPatchAll(typeof(Start));
+        }
+
+        [HarmonyPatch(typeof(MapCoin), "Start")]
+        internal static class Start {
+            static bool Prefix(string ___coinID, MapCoin __instance) {
+                Plugin.Log("Coin "+___coinID);
+                return false;
+            }
+        }
+    }
+}
---
+++ TODO.md
@@ -0,0 +1,4 @@
+## To Do
+
+- Make Level Checks work correctly.
+- Make Coins Work Correctly
