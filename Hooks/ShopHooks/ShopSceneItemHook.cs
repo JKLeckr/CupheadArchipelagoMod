172da670d37e28bd0564b3fd4ebb3c6b53f8cf4c
JKLeckr <11635283+JKLeckr@users.noreply.github.com>
2024-07-26 12:25:24 -0500

    Adjusted shop labels

--- Hooks/ShopHooks/ShopSceneItemHook.cs
+++ Hooks/ShopHooks/ShopSceneItemHook.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
 using System.Reflection;
 using System.Reflection.Emit;
 using Archipelago.MultiClient.Net.Enums;
