57cec1f690cf0bcf3b5985515b812c1a13651d1a
JKLeckr <11635283+JKLeckr@users.noreply.github.com>
2024-08-23 16:45:51 -0500

    Consolidating

--- AP/APItemData.cs
+++ AP/APItemData.cs
@@ -1,6 +1,7 @@
 /// Copyright 2024 JKLeckr
 /// SPDX-License-Identifier: Apache-2.0
 
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using Archipelago.MultiClient.Net.Models;
 
@@ -50,5 +51,14 @@ namespace CupheadArchipelago.AP {
                 return hash;
             }
         }
+    }
+
+    internal class APItemDataComparer(bool useId = true) : IEqualityComparer<APItemData> {
+        private readonly bool useId = useId;
+
+        public bool Equals(APItemData a, APItemData b) {
+            return (!useId || a.Id == b.Id) && a.Location == b.Location && a.Player == b.Player;
+        }
+        public int GetHashCode(APItemData item) => item.GetHashCode(useId);
     }
 }
