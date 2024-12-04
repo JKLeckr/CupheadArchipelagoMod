cd3d630d2ed51fa2d13279616be0adc564365f04
JKLeckr <11635283+JKLeckr@users.noreply.github.com>
2024-12-04 10:28:47 -0600

    Cleanup

--- Hooks/SaveKeyUpdaterHook.cs
+++ Hooks/SaveKeyUpdaterHook.cs
@@ -64,26 +64,23 @@ namespace CupheadArchipelago.Hooks {
         }
 
         private static List<CodeInstruction> DigestKeysInstructions(IEnumerable<CodeInstruction> instructions) {
-            List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
-            List<CodeInstruction> ncodes = new List<CodeInstruction>();
-
-            ncodes.Add(new CodeInstruction(OpCodes.Ldc_I4_3));
-            ncodes.Add(new CodeInstruction(OpCodes.Newarr, typeof(string)));
-
-            ncodes.Add(new CodeInstruction(OpCodes.Dup));
-            ncodes.Add(new CodeInstruction(OpCodes.Ldc_I4_0));
-            ncodes.Add(new CodeInstruction(OpCodes.Ldstr, SaveKeyNames[0]));
-            ncodes.Add(new CodeInstruction(OpCodes.Stelem_Ref));
-
-            ncodes.Add(new CodeInstruction(OpCodes.Dup));
-            ncodes.Add(new CodeInstruction(OpCodes.Ldc_I4_1));
-            ncodes.Add(new CodeInstruction(OpCodes.Ldstr, SaveKeyNames[1]));
-            ncodes.Add(new CodeInstruction(OpCodes.Stelem_Ref));
-
-            ncodes.Add(new CodeInstruction(OpCodes.Dup));
-            ncodes.Add(new CodeInstruction(OpCodes.Ldc_I4_2));
-            ncodes.Add(new CodeInstruction(OpCodes.Ldstr, SaveKeyNames[2]));
-            ncodes.Add(new CodeInstruction(OpCodes.Stelem_Ref));
+            List<CodeInstruction> codes = new(instructions);
+            List<CodeInstruction> ncodes = [
+                new CodeInstruction(OpCodes.Ldc_I4_3),
+                new CodeInstruction(OpCodes.Newarr, typeof(string)),
+                new CodeInstruction(OpCodes.Dup),
+                new CodeInstruction(OpCodes.Ldc_I4_0),
+                new CodeInstruction(OpCodes.Ldstr, SaveKeyNames[0]),
+                new CodeInstruction(OpCodes.Stelem_Ref),
+                new CodeInstruction(OpCodes.Dup),
+                new CodeInstruction(OpCodes.Ldc_I4_1),
+                new CodeInstruction(OpCodes.Ldstr, SaveKeyNames[1]),
+                new CodeInstruction(OpCodes.Stelem_Ref),
+                new CodeInstruction(OpCodes.Dup),
+                new CodeInstruction(OpCodes.Ldc_I4_2),
+                new CodeInstruction(OpCodes.Ldstr, SaveKeyNames[2]),
+                new CodeInstruction(OpCodes.Stelem_Ref),
+            ];
 
             for (int i=0;i<codes.Count-1;i++) {
                 if (codes[i].opcode == OpCodes.Ldsfld && (FieldInfo)codes[i].operand == _fi_SAVE_FILE_KEYS && codes[i+1].opcode != OpCodes.Ldlen) {
