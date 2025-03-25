5a9b2e6fa68827ac710b9fc725643cfae40c1215
JKLeckr <11635283+JKLeckr@users.noreply.github.com>
2025-03-24 21:31:52 -0500

    And now, consolodation

--- Config.cs
+++ Config.cs
@@ -1,9 +1,31 @@
 /// Copyright 2025 JKLeckr
 /// SPDX-License-Identifier: Apache-2.0
 
+using System;
 using CupheadArchipelago.AP;
 
 namespace CupheadArchipelago {
+    [Flags]
+    public enum Cutscenes {
+        None = 0,
+        Intro = 1,
+        KettleIntro = 2,
+        //FullIntro = 3,
+        DieHouseCutscenes = 4,
+        EndCutscene = 64,
+        DLCIntro = 256, //TODO Add
+        DLCSaltbakerIntro = 512, //TODO Add
+        //DLCFullIntro = 768,
+        DLCEndCutscene = 8192, //TODO Add
+        //All = 32767,
+    }
+
+    [Flags]
+    public enum APStatsFunctions {
+        None = 0,
+        ConnectionIndicator = 1,
+    }
+
     public class Config {
         private static Config current;
 
