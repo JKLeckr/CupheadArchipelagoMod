48ebbef63a0ea3515af9cad7e74f3a82f17a16bf
JKLeckr <11635283+JKLeckr@users.noreply.github.com>
2025-03-31 01:02:16 -0500

    Added WIP Ingredients HUD

--- Hooks/MapHooks/MapUICoinsHook.cs
+++ Hooks/MapHooks/MapUICoinsHook.cs
@@ -7,16 +7,35 @@ using System.Reflection;
 using System.Reflection.Emit;
 using CupheadArchipelago.AP;
 using HarmonyLib;
+using UnityEngine;
 
 namespace CupheadArchipelago.Hooks.MapHooks {
     internal class MapUICoinsHook {
         internal static void Hook() {
-            //Harmony.CreateAndPatchAll(typeof(Start));
+            Harmony.CreateAndPatchAll(typeof(Start));
             Harmony.CreateAndPatchAll(typeof(Update));
         }
 
         [HarmonyPatch(typeof(MapUICoins), "Start")]
-        internal static class Start {}
+        internal static class Start {
+            static void Postfix(MapUICoins __instance, ref PlayerId ___playerId) {
+                if (!APData.IsCurrentSlotEnabled() || !APSettings.UseDLC) return;
+                if (__instance.name == "CurrencyCanvasTwo") {
+                    /*GameObject currencyOne = GameObject.Find("CurrencyCanvasOne");
+                    Vector2 diff = __instance.GetComponent<RectTransform>().anchoredPosition - currencyOne.GetComponent<RectTransform>().anchoredPosition;
+                    Logging.Log($"HUD Diff: {diff.x}, {diff.y}");*/
+                    GameObject currencyThree = UnityEngine.Object.Instantiate(__instance.gameObject, __instance.transform.parent);
+                    currencyThree.name = "CurrencyCanvasThree";
+                    currencyThree.transform.SetSiblingIndex(2);
+                    RectTransform rect = currencyThree.GetComponent<RectTransform>();
+                    rect.anchoredPosition += new Vector2(108.2598f, 0f);
+                    Logging.Log("Initialized Ingredients HUD");
+                }
+                else if (__instance.name == "CurrencyCanvasThree") {
+                    ___playerId = PlayerId.Any;
+                }
+            }
+        }
 
         [HarmonyPatch(typeof(MapUICoins), "Update")]
         internal static class Update {
@@ -69,8 +88,8 @@ namespace CupheadArchipelago.Hooks.MapHooks {
                 if (APData.IsCurrentSlotEnabled() && playerId != PlayerId.PlayerOne) {
                     if (playerId == PlayerId.PlayerTwo)
                         return APData.CurrentSData.playerData.contracts;
-                    /*if (playerId == PlayerId.Any)
-                        return APData.CurrentSData.playerData.dlc_ingredients;*/
+                    if (playerId == PlayerId.Any)
+                        return APData.CurrentSData.playerData.dlc_ingredients;
                 }
                 return PlayerData.Data.GetCurrency(playerId);
             }
