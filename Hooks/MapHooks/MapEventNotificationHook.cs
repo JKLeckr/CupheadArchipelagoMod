76522f10cfea891ec924e159b17d41eaab043204
JKLeckr <11635283+JKLeckr@users.noreply.github.com>
2025-04-01 15:09:33 -0500

    Fix

--- Hooks/MapHooks/MapEventNotificationHook.cs
+++ Hooks/MapHooks/MapEventNotificationHook.cs
@@ -1,13 +1,23 @@
 /// Copyright 2025 JKLeckr
 /// SPDX-License-Identifier: Apache-2.0
 
+using CupheadArchipelago.AP;
 using HarmonyLib;
 
 namespace CupheadArchipelago.Hooks.MapHooks {
     internal class MapEventNotificationHook {
         // TODO: Add notifications for sending and maybe receiving checks too!
-        internal static void Hook() {}
+        internal static void Hook() {
+            Harmony.CreateAndPatchAll(typeof(ShowTooltipEvent));
+        }
 
-        internal static class ShowTooltipEvent {}
+        [HarmonyPatch(typeof(MapEventNotification), "ShowTooltipEvent")]
+        internal static class ShowTooltipEvent {
+            static bool Prefix(TooltipEvent tooltipEvent) {
+                if (APData.IsCurrentSlotEnabled() && tooltipEvent == TooltipEvent.BackToKitchen)
+                    return false;
+                else return true;
+            }
+        }
     }
 }
