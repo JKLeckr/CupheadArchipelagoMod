f89b6b28cb10ade392ba53a788ffc674a0e6f86a
JKLeckr <11635283+JKLeckr@users.noreply.github.com>
2025-04-07 12:36:17 -0500

    Made slotdata casting efficient and reliable

--- AP/APSlotData.cs
+++ AP/APSlotData.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using CupheadArchipelago.Util;
 using Newtonsoft.Json;
 using FVer;
 
@@ -37,15 +38,15 @@ namespace CupheadArchipelago.AP {
         internal readonly bool deathlink;
 
         internal APSlotData(Dictionary<string, object> slotData) {
-            version = GetAPSlotDataValueLong(slotData, "version");
+            version = GetAPSlotDataValue<long>(slotData, "version");
             world_version = GetAPSlotDataVersion(slotData, "world_version");
             level_shuffle_map = new LevelShuffleMap(GetAPSlotDataDeserializedValue<Dictionary<long, long>>(slotData, "level_shuffle_map"));
             shop_map = GetAPShopMap(slotData);
             //Logging.Log($"shop_map: {shop_map}");
-            use_dlc = GetAPSlotDataValueBool(slotData, "use_dlc");
+            use_dlc = GetAPSlotDataValue<bool>(slotData, "use_dlc");
             mode = GetAPSlotDataValue<GameMode>(slotData, "mode");
-            expert_mode = GetAPSlotDataValueBool(slotData, "expert_mode");
-            start_weapon = GetAPSlotDataValueLong(slotData, "start_weapon") switch {
+            expert_mode = GetAPSlotDataValue<bool>(slotData, "expert_mode");
+            start_weapon = GetAPSlotDataValue<sbyte>(slotData, "start_weapon") switch {
                 1 => APItem.weapon_spread,
                 2 => APItem.weapon_chaser,
                 3 => APItem.weapon_lobber,
@@ -62,39 +63,48 @@ namespace CupheadArchipelago.AP {
             dlc_ingredient_requirements = GetAPSlotDataValue<int>(slotData, "dlc_ingredient_requirements");
             contract_goal_requirements = GetAPSlotDataValue<int>(slotData, "contract_goal_requirements");
             dlc_ingredient_goal_requirements = GetAPSlotDataValue<int>(slotData, "dlc_ingredient_goal_requirements");
-            freemove_isles = GetAPSlotDataValueBool(slotData, "freemove_isles");
-            randomize_abilities = GetAPSlotDataValueBool(slotData, "randomize_abilities");
-            randomize_abilities_aim = GetAPSlotDataValueBool(slotData, "randomize_abilities_aim");
+            freemove_isles = GetAPSlotDataValue<bool>(slotData, "freemove_isles");
+            randomize_abilities = GetAPSlotDataValue<bool>(slotData, "randomize_abilities");
+            randomize_abilities_aim = false; //GetAPSlotDataValue<bool>(slotData, "randomize_abilities_aim");
             boss_grade_checks = GetAPSlotDataValue<GradeChecks>(slotData, "boss_grade_checks");
             rungun_grade_checks = GetAPSlotDataValue<GradeChecks>(slotData, "rungun_grade_checks");
             start_maxhealth = GetAPSlotDataValue<int>(slotData, "start_maxhealth");
             dlc_chalice = GetAPSlotDataValue<DlcChaliceMode>(slotData, "dlc_chalice");
             dlc_curse_mode = GetAPSlotDataValue<DlcCurseMode>(slotData, "dlc_curse_mode");
-            trap_loadout_anyweapon = GetAPSlotDataValueBool(slotData, "trap_loadout_anyweapon");
+            trap_loadout_anyweapon = GetAPSlotDataValue<bool>(slotData, "trap_loadout_anyweapon");
             music_shuffle = 0; //GetOptionalAPSlotDataValue<MusicGroups>(slotData, "music_rando", 0);
-            deathlink = GetAPSlotDataValueBool(slotData, "deathlink");
+            deathlink = GetAPSlotDataValue<bool>(slotData, "deathlink");
         }
 
-        public static long GetSlotDataVersion(Dictionary<string, object> slotData) => GetAPSlotDataValueLong(slotData, "version");
-        public static string GetAPWorldVersionString(Dictionary<string, object> slotData) => GetAPSlotDataValueString(slotData, "world_version");
-        private static bool GetAPSlotDataValueBool(Dictionary<string, object> slotData, string key) => Convert.ToBoolean(GetAPSlotDataValueLong(slotData, key));
-        private static long GetAPSlotDataValueLong(Dictionary<string, object> slotData, string key) => GetAPSlotDataValue<long>(slotData, key);
-        private static string GetAPSlotDataValueString(Dictionary<string, object> slotData, string key) => GetAPSlotDataValue<object>(slotData, key).ToString();
+        public static long GetSlotDataVersion(Dictionary<string, object> slotData) => GetAPSlotDataValue<long>(slotData, "version");
+        public static string GetAPWorldVersionString(Dictionary<string, object> slotData) => GetAPSlotDataValue<string>(slotData, "world_version");
+        
         private static T GetAPSlotDataDeserializedValue<T>(Dictionary<string, object> slotData, string key) {
             try {
-                string raw = GetAPSlotDataValueString(slotData, key);
+                string raw = GetAPSlotDataValue<string>(slotData, key);
                 //Logging.Log($"[APSlotData]: Deserializing: \"{raw}\"");
                 return JsonConvert.DeserializeObject<T>(raw);
             } catch (Exception e) {
-                throw new ArgumentException($"Invalid SlotData data for: \"{key}\"! Exception: {e}");
+                throw new ArgumentException($"GetAPSlotDataDeserializedValue: Invalid SlotData data for: \"{key}\"! Exception: {e}");
+            }
+        }
+        private static T GetOptionalAPSlotDataDeserializedValue<T>(Dictionary<string, object> slotData, string key, T fallbackValue) {
+            try {
+                string raw = GetAPSlotDataValue<string>(slotData, key);
+                //Logging.Log($"[APSlotData]: Deserializing: \"{raw}\"");
+                return JsonConvert.DeserializeObject<T>(raw);
+            } catch (Exception e) {
+                Logging.LogWarning($"GetOptionalAPSlotDataDeserializedValue: Invalid SlotData data for: \"{key}\"! Exception: {e}");
+                Logging.LogWarning($"Using fallback value of \"{fallbackValue}\" for \"{key}\"");
+                return fallbackValue;
             }
         }
         private static T GetAPSlotDataValue<T>(Dictionary<string, object> slotData, string key) {
             if (slotData.ContainsKey(key)) {
                 try {
-                    return CastAPSlotDataValue<T>(slotData[key]);
+                    return Converter.ConvertTo<T>(slotData[key]);
                 } catch (InvalidCastException e) {
-                    throw new InvalidCastException($"GetOptionalAPSlotData: Could not get value from \"{key}\". Exception: {e.Message}");
+                    throw new InvalidCastException($"GetAPSlotData: Could not get value from \"{key}\". Exception: {e.Message}");
                 }
             } else {
                 throw new KeyNotFoundException($"GetAPSlotDataValue: \"{key}\" is not a valid key!");
@@ -103,7 +113,7 @@ namespace CupheadArchipelago.AP {
         private static T GetOptionalAPSlotDataValue<T>(Dictionary<string, object> slotData, string key, T fallbackValue) {
             if (slotData.ContainsKey(key)) {
                 try {
-                    return CastAPSlotDataValue<T>(slotData[key]);
+                    return Converter.ConvertTo<T>(slotData[key]);
                 } catch (InvalidCastException e) {
                     Logging.LogWarning($"GetOptionalAPSlotDataValue: Could not get value from \"{key}\". Exception: {e.Message}");
                 }
@@ -113,21 +123,8 @@ namespace CupheadArchipelago.AP {
             Logging.LogWarning($"Using fallback value of \"{fallbackValue}\" for \"{key}\"");
             return fallbackValue;
         }
-        private static T CastAPSlotDataValue<T>(object value) {
-            Type T_Type = typeof(T);
-            if (T_Type.IsEnum) {
-                return (T)Enum.Parse(T_Type, value.ToString());
-            }
-            if (T_Type == typeof(string)) {
-                return (T)(object)value.ToString();
-            }
-            if (T_Type == typeof(bool)) {
-                return (T)(object)Convert.ToBoolean((long)value);
-            }
-            return (T)value;
-        }
         private static FVersion GetAPSlotDataVersion(Dictionary<string, object> slotData, string key) {
-            string vraw = GetAPSlotDataValueString(slotData, key);
+            string vraw = GetAPSlotDataValue<string>(slotData, key);
             string[] mainParts = vraw.Split(['-'], 2);
             string[] versionParts = mainParts[0].Split(['.'], 4);
             FVersion res;
---
+++ Util/Convert.cs
@@ -0,0 +1,39 @@
+/// Copyright 2025 JKLeckr
+/// SPDX-License-Identifier: Apache-2.0
+
+using System;
+
+namespace CupheadArchipelago.Util {
+    internal static class Converter {
+        private static class TypeConverter<T> {
+            internal static readonly Func<object, T> ConvertTo;
+
+            static TypeConverter() {
+                Type T_Type = typeof(T);
+
+                if (T_Type == typeof(string)) {
+                    ConvertTo = value => (T)(object)value.ToString();
+                }
+                else if (T_Type == typeof(bool)) {
+                    ConvertTo = value => (T)(object)System.Convert.ToBoolean((long)value);
+                }
+                else if (T_Type == typeof(sbyte)) {
+                    ConvertTo = value => (T)(object)System.Convert.ToSByte(value);
+                }
+                else if (T_Type == typeof(int)) {
+                    ConvertTo = value => (T)(object)System.Convert.ToInt32(value);
+                }
+                else if (T_Type.IsEnum) {
+                    ConvertTo = value => (T)Enum.Parse(T_Type, value.ToString());
+                }
+                else {
+                    ConvertTo = value => (T)value;
+                }
+            }
+        }
+
+        internal static T ConvertTo<T>(this object value) {
+            return TypeConverter<T>.ConvertTo(value);
+        }
+    }
+}
