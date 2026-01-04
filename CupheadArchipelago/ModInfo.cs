edc9f18a3a9cd4f823fc8e511aa11668e980da82
JKLeckr <11635283+JKLeckr@users.noreply.github.com>
2026-01-04 00:39:42 -0600

    Updated third party license

--- CupheadArchipelago/ModInfo.cs
+++ CupheadArchipelago/ModInfo.cs
@@ -145,9 +145,42 @@ c-wspp websocket-sharp and c-wspp
 
     c-wspp also includes:
 
+        zlib
+
+            zlib.h -- interface of the 'zlib' general purpose compression library
+            version 1.3.1, January 22nd, 2024
+
+            Copyright (C) 1995-2024 Jean-loup Gailly and Mark Adler
+
+            This software is provided 'as-is', without any express or implied
+            warranty.  In no event will the authors be held liable for any damages
+            arising from the use of this software.
+
+            Permission is granted to anyone to use this software for any purpose,
+            including commercial applications, and to alter it and redistribute it
+            freely, subject to the following restrictions:
+
+            1. The origin of this software must not be misrepresented; you must not
+               claim that you wrote the original software. If you use this software
+               in a product, an acknowledgment in the product documentation would be
+               appreciated but is not required.
+            2. Altered source versions must be plainly marked as such, and must not be
+               misrepresented as being the original software.
+            3. This notice may not be removed or altered from any source distribution.
+
+            Jean-loup Gailly        Mark Adler
+            jloup@gzip.org          madler@alumni.caltech.edu
+
+
+        OpenSSL
+
+            Licensed under the Apache 2.0 License.
+            See license at https://openssl-library.org/source/license/apache-license-2.0.txt
+
+
         ASIO
 
-            Copyright (c) 2003-2021 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+            Copyright (c) 2003-2024 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 
             Distributed under the Boost Software License, Version 1.0. (See accompanying
             file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
