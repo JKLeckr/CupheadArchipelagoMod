957e0198f8f5b5572a2e97cc8cc4584206bc122f
JKLeckr <11635283+JKLeckr@users.noreply.github.com>
2026-07-24 19:34:05 -0500

    Added safeguard for coins already collected, but not checked

--- CupheadArchipelago/Hooks/LevelHooks/LevelCoinHook.cs
+++ CupheadArchipelago/Hooks/LevelHooks/LevelCoinHook.cs
@@ -25,17 +25,57 @@ namespace CupheadArchipelago.Hooks.LevelHooks {
                 return true;
             }*/
 
-            static void Postfix(LevelCoin __instance, ref bool ____collected) {
-                if (APData.IsCurrentSlotEnabled()) {
-                    if (APClient.IsLocationChecked(CoinIdMap.GetAPLocation(__instance.GlobalID))) {
-                        Logging.LogDebug($"{__instance.GlobalID} {GetLocationId(__instance.GlobalID)} already Collected");
-                        PlayerData.Data.coinManager.SetCoinValue(__instance.GlobalID, true, PlayerId.PlayerOne);
-                        ____collected = true;
-                        UnityEngine.Object.Destroy(__instance.gameObject);
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+                List<CodeInstruction> codes = new(instructions);
+                bool success = false;
+                bool debug = false;
+
+                MethodInfo _mi_get_Data = typeof(PlayerData).GetProperty("Data", BindingFlags.Public | BindingFlags.Static)?.GetGetMethod();
+                MethodInfo _mi_GetCoinCollected = typeof(PlayerData).GetMethod("GetCoinCollected", BindingFlags.Public | BindingFlags.Instance);
+                MethodInfo _mi_APCondition = typeof(Awake).GetMethod("APCondition", BindingFlags.NonPublic | BindingFlags.Static);
+
+                if (debug) {
+                    Dbg.LogCodeInstructions(codes);
+                }
+                for (int i = 0; i < codes.Count - 3; i++) {
+                    if (codes[i].opcode == OpCodes.Call && (MethodInfo)codes[i].operand == _mi_get_Data && codes[i + 1].opcode == OpCodes.Ldarg_0 &&
+                        codes[i + 2].opcode == OpCodes.Callvirt && (MethodInfo)codes[i + 2].operand == _mi_GetCoinCollected && codes[i + 3].opcode == OpCodes.Brfalse
+                    ) {
+                        List<CodeInstruction> ncodes = [
+                            new(OpCodes.Ldarg_0),
+                            new(OpCodes.Call, _mi_APCondition)
+                        ];
+
+                        codes.InsertRange(i + 3, ncodes);
+
+                        success = true;
+                        break;
                     }
                 }
+                if (!success) throw new Exception($"{nameof(Awake)}: Patch Failed!");
+                if (debug) {
+                    Logging.Log("---");
+                    Dbg.LogCodeInstructions(codes);
+                }
+
+                return codes;
             }
 
+            static void Postfix(LevelCoin __instance, bool ____collected) {
+                Logging.Log($"{CoinIdMap.GetAPLocation(__instance.GlobalID)} got: {____collected}");
+            }
+
+            private static bool APCondition(bool orig, LevelCoin instance) {
+                if (APData.IsCurrentSlotEnabled()) {
+                    if (APClient.IsLocationChecked(CoinIdMap.GetAPLocation(instance.GlobalID))) {
+                        Logging.LogDebug($"{instance.GlobalID} {GetLocationId(instance.GlobalID)} already Collected");
+                        PlayerData.Data.coinManager.SetCoinValue(instance.GlobalID, true, PlayerId.PlayerOne);
+                        return true;
+                    }
+                    return false;
+                }
+                return orig;
+            }
             private static long GetLocationId(string coinId) {
                 if (CoinIdMap.CoinIDExists(coinId)) {
                     return CoinIdMap.GetAPLocation(coinId);
